0766b37aee149810e73cabb9953cba70aab6d5dc
Marcelo Cohen <mflashbr@gmail.com>
2025-10-01 11:09:54 -0300

    Código do movimento de tiro do inimigo

---
+++ enemy_bullet.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var speed = 150
+
+func start(pos):
+	position = pos
+
+func _process(delta):
+	position.y += speed * delta
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.name == "player":
+		queue_free()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
